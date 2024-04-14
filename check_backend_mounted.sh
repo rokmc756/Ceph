7df1bbb8e94abd94459529067e5670375ce33751
Jack Moon <rokmc756@gmail.com>
2024-04-15 02:30:06 +0900

    Updated for Installing and Uninstalling CephFS Clients

---
+++ check_backend_mounted.sh
@@ -0,0 +1,9 @@
+for i in `seq 1 6`
+do
+
+    ssh root@192.168.0.7$i "mount -l | grep -E 'nfs|smb|cephfs'"
+
+done
+
+# for i in `find ./ -name "*2023-03-21*.csv"`; do grep --with-filename -E 'FATAL\:|ERROR\:' $i; done
+
---
+++ check_clients_mounted.sh
@@ -0,0 +1,9 @@
+for i in `seq 1 3`
+do
+
+    ssh root@192.168.0.6$i "mount -l | grep -E 'nfs|smb|cepfs'"
+
+done
+
+# for i in `find ./ -name "*2023-03-21*.csv"`; do grep --with-filename -E 'FATAL\:|ERROR\:' $i; done
+
